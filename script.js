005fd6f6b5a3b180c843c335a8de5f7c9af9890a
webwork34 <54593230+webwork34@users.noreply.github.com>
2019-09-28 14:11:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ let counter      = true,
 btn.addEventListener('click', function(){
     if(counter){
         box1.style.display = 'block';
-        box2.style.color = 'white';
         box2.style.display = 'none';
         X.style.display = 'block';
         counter = false;
     }else{
         box1.style.display = 'none';
-        box2.style.color = '#372243';
         box2.style.display = 'block';
         X.style.display = 'none';
         counter = true;
